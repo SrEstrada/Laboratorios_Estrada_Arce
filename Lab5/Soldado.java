22bdc818983164b648d13b9926c4a1dbf2bfcb52
SrEstrada <sestradaa@unsa.edu.pe>
2024-10-18 18:12:00 -0500

    Creación de la clase Soldado con atributos nombre, vida, fila y columna. Incluye constructor, getters, setters y método toString para mostrar la información del soldado.

---
+++ Lab5/Soldado.java
@@ -0,0 +1,43 @@
+package Ejercicio;
+public class Soldado {
+    private String nombre;
+    private int vida;
+    private int fila;
+    private int columna;
+    // Constructor
+    public Soldado(String nombre, int vida, int fila, int columna) {
+        this.nombre = nombre;
+        this.vida = vida;
+        this.fila = fila;
+        this.columna = columna;
+    }
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getVida() {
+        return vida;
+    }
+
+    public int getFila() {
+        return fila;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+    // Setters 
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    // Método toString para mostrar la información del soldado
+    @Override
+    public String toString() {
+        return "Soldado{" +
+                "nombre='" + nombre + '\'' +
+                ", vida=" + vida +
+                ", posición=(" + fila + ", " + columna + ")" +
+                '}';
+    }
+}

076b1be40547060ec40367e180a58016f3e255de
SrEstrada <sestradaa@unsa.edu.pe>
2024-11-29 18:56:30 -0500

    Modifiqué inicializarEjercitos para que utilice HashMap en lugar de arreglos

--- Lab8/VideoJuego.java
+++ Lab8/VideoJuego.java
@@ -11,36 +11,36 @@ public class VideoJuego {
         ejercito2 = new HashMap<>();
         inicializarEjercitos();
     }
-
-    private void inicializarEjercitos() {
-        Random rand = new Random();
-        int numSoldados1 = rand.nextInt(5) + 1; 
-        for (int i = 0; i < numSoldados1; i++) {
-            String nombre = "Soldado1_" + i;
-            int nivelVida = rand.nextInt(5) + 1;
-            int fila, columna;
-            do {
-                fila = rand.nextInt(tablero.length);
-                columna = rand.nextInt(tablero[0].length);
-            } while (tablero[fila][columna] != null);
-            Soldado nuevoSoldado = new Soldado(nombre, nivelVida, fila, columna);
-            tablero[fila][columna] = nuevoSoldado;
-            ejercito1[i] = nuevoSoldado;
-        }
-        int numSoldados2 = rand.nextInt(5) + 1;
-        for (int i = 0; i < numSoldados2; i++) {
-            String nombre = "Soldado2_" + i;
-            int nivelVida = rand.nextInt(5) + 1;
-            int fila, columna;
-            do {
-                fila = rand.nextInt(tablero.length);
-                columna = rand.nextInt(tablero[0].length);
-            } while (tablero[fila][columna] != null);
-            Soldado nuevoSoldado = new Soldado(nombre, nivelVida, fila, columna);
-            tablero[fila][columna] = nuevoSoldado;
-            ejercito2[i] = nuevoSoldado;
-        }
+private void inicializarEjercitos() {
+    Random rand = new Random();
+    int numSoldados1 = rand.nextInt(10) + 1;
+    for (int i = 0; i < numSoldados1; i++) {
+        String nombre = "Soldado1_" + i;
+        int nivelVida = rand.nextInt(5) + 1;
+        int fila, columna;
+        do {
+            fila = rand.nextInt(tablero.length);
+            columna = rand.nextInt(tablero[0].length);
+        } while (tablero[fila][columna] != null);
+        Soldado nuevoSoldado = new Soldado(nombre, nivelVida, fila, columna);
+        tablero[fila][columna] = nuevoSoldado;
+        ejercito1.put(nombre, nuevoSoldado);
     }
+
+    int numSoldados2 = rand.nextInt(10) + 1;
+    for (int i = 0; i < numSoldados2; i++) {
+        String nombre = "Soldado2_" + i;
+        int nivelVida = rand.nextInt(5) + 1;
+        int fila, columna;
+        do {
+            fila = rand.nextInt(tablero.length);
+            columna = rand.nextInt(tablero[0].length);
+        } while (tablero[fila][columna] != null);
+        Soldado nuevoSoldado = new Soldado(nombre, nivelVida, fila, columna);
+        tablero[fila][columna] = nuevoSoldado;
+        ejercito2.put(nombre, nuevoSoldado);
+    }
+}
 
     public void mostrarTablero() {
         System.out.print("   ");
